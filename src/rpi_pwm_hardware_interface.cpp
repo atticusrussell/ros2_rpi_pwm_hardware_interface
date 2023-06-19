263768259464afc5da9c66effebb4383bb8e9cd5
Atticus Russell <atticusrussell@gmail.com>
2023-06-18 23:48:13 -0400

    now at real 3 spaces

--- src/rpi_pwm_hardware_interface.cpp
+++ src/rpi_pwm_hardware_interface.cpp
@@ -25,110 +25,110 @@
 
 namespace rpi_pwm_hardware_interface {
 hardware_interface::CallbackReturn RPiPWMHardwareInterface::on_init(
-            const hardware_interface::HardwareInfo& info) {
-    if (hardware_interface::SystemInterface::on_init(info) != CallbackReturn::SUCCESS) {
-        return CallbackReturn::ERROR;
-    }
+         const hardware_interface::HardwareInfo& info) {
+   if (hardware_interface::SystemInterface::on_init(info) != CallbackReturn::SUCCESS) {
+      return CallbackReturn::ERROR;
+   }
 
-    // TEST(anyone): read parameters and initialize the hardware
-    rudder_joint_.name = info_.hardware_parameters["srv_name"];
-    srv_cfg_.pin = std::stoi(info_.hardware_parameters["srv_pin"]);
-    srv_cfg_.min_angle = std::stof(info_.hardware_parameters["srv_min_angle"]);
-    srv_cfg_.max_angle = std::stof(info_.hardware_parameters["srv_max_angle"]);
-    srv_cfg_.min_pulse_width_us = std::stoi(info_.hardware_parameters["srv_min_pulse_width_us"]);
-    srv_cfg_.max_pulse_width_us = std::stoi(info_.hardware_parameters["srv_max_pulse_width_us"]);
+   // TEST(anyone): read parameters and initialize the hardware
+   rudder_joint_.name = info_.hardware_parameters["srv_name"];
+   srv_cfg_.pin = std::stoi(info_.hardware_parameters["srv_pin"]);
+   srv_cfg_.min_angle = std::stof(info_.hardware_parameters["srv_min_angle"]);
+   srv_cfg_.max_angle = std::stof(info_.hardware_parameters["srv_max_angle"]);
+   srv_cfg_.min_pulse_width_us = std::stoi(info_.hardware_parameters["srv_min_pulse_width_us"]);
+   srv_cfg_.max_pulse_width_us = std::stoi(info_.hardware_parameters["srv_max_pulse_width_us"]);
 
-    return CallbackReturn::SUCCESS;
+   return CallbackReturn::SUCCESS;
 }
 
 hardware_interface::CallbackReturn RPiPWMHardwareInterface::on_configure(
-            const rclcpp_lifecycle::State& /*previous_state*/) {
-    // TODO(anyone): prepare the robot to be ready for read calls and write
-    // calls of some interfaces
-    if (!isPigpiodRunning()) {
-        startPigpiod();
-    }
+         const rclcpp_lifecycle::State& /*previous_state*/) {
+   // TODO(anyone): prepare the robot to be ready for read calls and write
+   // calls of some interfaces
+   if (!isPigpiodRunning()) {
+      startPigpiod();
+   }
 
-    if (isPigpiodRunning()) {
-        // pigpiod daemon is running
-        return CallbackReturn::SUCCESS;
-    } else {
-        // pigpiod daemon is not running
-        return CallbackReturn::ERROR;
-    }
+   if (isPigpiodRunning()) {
+      // pigpiod daemon is running
+      return CallbackReturn::SUCCESS;
+   } else {
+      // pigpiod daemon is not running
+      return CallbackReturn::ERROR;
+   }
 }
 
 hardware_interface::CallbackReturn RPiPWMHardwareInterface::on_cleanup(
-            const rclcpp_lifecycle::State& /*previous_state*/) {
-    // TEST(anyone): free resources, stop threads, etc.
-    pigpio_stop(rudder_joint_.servo->__pi);
-    return CallbackReturn::SUCCESS;
+         const rclcpp_lifecycle::State& /*previous_state*/) {
+   // TEST(anyone): free resources, stop threads, etc.
+   pigpio_stop(rudder_joint_.servo->__pi);
+   return CallbackReturn::SUCCESS;
 }
 
 std::vector<hardware_interface::StateInterface> RPiPWMHardwareInterface::export_state_interfaces() {
-    std::vector<hardware_interface::StateInterface> state_interfaces;
-    state_interfaces.emplace_back(hardware_interface::StateInterface(rudder_joint_.name,
-                hardware_interface::HW_IF_POSITION, &rudder_joint_.pos));
-    // TEST(anyone): insert correct interfaces
+   std::vector<hardware_interface::StateInterface> state_interfaces;
+   state_interfaces.emplace_back(hardware_interface::StateInterface(rudder_joint_.name,
+            hardware_interface::HW_IF_POSITION, &rudder_joint_.pos));
+   // TEST(anyone): insert correct interfaces
 
-    return state_interfaces;
+   return state_interfaces;
 }
 
 std::vector<hardware_interface::CommandInterface>
 RPiPWMHardwareInterface::export_command_interfaces() {
-    std::vector<hardware_interface::CommandInterface> command_interfaces;
-    command_interfaces.emplace_back(hardware_interface::CommandInterface(rudder_joint_.name,
-                hardware_interface::HW_IF_POSITION, &rudder_joint_.cmd));
-    // TEST(anyone): insert correct interfaces
+   std::vector<hardware_interface::CommandInterface> command_interfaces;
+   command_interfaces.emplace_back(hardware_interface::CommandInterface(rudder_joint_.name,
+            hardware_interface::HW_IF_POSITION, &rudder_joint_.cmd));
+   // TEST(anyone): insert correct interfaces
 
-    return command_interfaces;
+   return command_interfaces;
 }
 
 hardware_interface::CallbackReturn RPiPWMHardwareInterface::on_activate(
-            const rclcpp_lifecycle::State& /*previous_state*/) {
-    // TEST(anyone): prepare the robot to receive commands
-    rudder_joint_.name = srv_cfg_.name;
-    srv_cfg_.pi = pigpio_start(NULL, NULL);
-    if (srv_cfg_.pi < 0) {
-        // TODO(anyone) maybe make ros log error
-        std::cerr << "Error initializing pigpio" << std::endl;
-        return CallbackReturn::ERROR;
-    } else {
-        rudder_joint_.servo = std::make_unique<AngularServo>(srv_cfg_.pi, srv_cfg_.pin,
-                    srv_cfg_.min_angle, srv_cfg_.max_angle, srv_cfg_.min_pulse_width_us,
-                    srv_cfg_.max_pulse_width_us);
-        return CallbackReturn::SUCCESS;
-    }
+         const rclcpp_lifecycle::State& /*previous_state*/) {
+   // TEST(anyone): prepare the robot to receive commands
+   rudder_joint_.name = srv_cfg_.name;
+   srv_cfg_.pi = pigpio_start(NULL, NULL);
+   if (srv_cfg_.pi < 0) {
+      // TODO(anyone) maybe make ros log error
+      std::cerr << "Error initializing pigpio" << std::endl;
+      return CallbackReturn::ERROR;
+   } else {
+      rudder_joint_.servo = std::make_unique<AngularServo>(srv_cfg_.pi, srv_cfg_.pin,
+               srv_cfg_.min_angle, srv_cfg_.max_angle, srv_cfg_.min_pulse_width_us,
+               srv_cfg_.max_pulse_width_us);
+      return CallbackReturn::SUCCESS;
+   }
 }
 
 hardware_interface::CallbackReturn RPiPWMHardwareInterface::on_deactivate(
-            const rclcpp_lifecycle::State& /*previous_state*/) {
-    // TEST(anyone): prepare the robot to stop receiving commands
-    // FUTURE(anyone) maybe make a destructor for AngularServo?
-    rudder_joint_.servo->setPulseWidth(0);
+         const rclcpp_lifecycle::State& /*previous_state*/) {
+   // TEST(anyone): prepare the robot to stop receiving commands
+   // FUTURE(anyone) maybe make a destructor for AngularServo?
+   rudder_joint_.servo->setPulseWidth(0);
 
-    return CallbackReturn::SUCCESS;
+   return CallbackReturn::SUCCESS;
 }
 
 hardware_interface::return_type RPiPWMHardwareInterface::read(const rclcpp::Time& /*time*/,
-            const rclcpp::Duration& /*period*/) {
-    // TEST(anyone): read robot states
-    float currentAngleDegrees = rudder_joint_.servo->getAngle();
-    // convert from degrees to radians
-    rudder_joint_.pos = currentAngleDegrees * M_PI / 180;
+         const rclcpp::Duration& /*period*/) {
+   // TEST(anyone): read robot states
+   float currentAngleDegrees = rudder_joint_.servo->getAngle();
+   // convert from degrees to radians
+   rudder_joint_.pos = currentAngleDegrees * M_PI / 180;
 
-    return hardware_interface::return_type::OK;
+   return hardware_interface::return_type::OK;
 }
 
 hardware_interface::return_type RPiPWMHardwareInterface::write(const rclcpp::Time& /*time*/,
-            const rclcpp::Duration& /*period*/) {
-    // TEST(anyone): write robot's commands'
-    // FUTURE change function names to setAngleDeg and getAngleDeg
-    // convert from radians to degrees
-    float desiredAngleDegrees = rudder_joint_.cmd * 180 / M_PI;
-    rudder_joint_.servo->setAngle(desiredAngleDegrees);
+         const rclcpp::Duration& /*period*/) {
+   // TEST(anyone): write robot's commands'
+   // FUTURE change function names to setAngleDeg and getAngleDeg
+   // convert from radians to degrees
+   float desiredAngleDegrees = rudder_joint_.cmd * 180 / M_PI;
+   rudder_joint_.servo->setAngle(desiredAngleDegrees);
 
-    return hardware_interface::return_type::OK;
+   return hardware_interface::return_type::OK;
 }
 
 }  // namespace rpi_pwm_hardware_interface
@@ -136,4 +136,4 @@ hardware_interface::return_type RPiPWMHardwareInterface::write(const rclcpp::Tim
 #include "pluginlib/class_list_macros.hpp"
 
 PLUGINLIB_EXPORT_CLASS(rpi_pwm_hardware_interface::RPiPWMHardwareInterface,
-            hardware_interface::SystemInterface)
+         hardware_interface::SystemInterface)
--- test/test_rpi_pwm_hardware_interface.cpp
+++ test/test_rpi_pwm_hardware_interface.cpp
@@ -22,10 +22,10 @@
 #include "ros2_control_test_assets/descriptions.hpp"
 
 class TestRPiPWMHardwareInterface : public ::testing::Test {
-    protected:
-        void SetUp() override {
-            // TODO(anyone): Extend this description to your robot
-            rpi_pwm_hardware_interface_2dof_ = R"(
+   protected:
+      void SetUp() override {
+         // TODO(anyone): Extend this description to your robot
+         rpi_pwm_hardware_interface_2dof_ = R"(
                   <ros2_control name="RPiPWMHardwareInterface2dof" type="system">
                      <hardware>
                         <plugin>rpi_pwm_hardware_interface/RPiPWMHardwareInterface</plugin>
@@ -42,13 +42,13 @@ class TestRPiPWMHardwareInterface : public ::testing::Test {
                      </joint>
                   </ros2_control>
                   )";
-        }
+      }
 
-        std::string rpi_pwm_hardware_interface_2dof_;
+      std::string rpi_pwm_hardware_interface_2dof_;
 };
 
 TEST_F(TestRPiPWMHardwareInterface, load_rpi_pwm_hardware_interface_2dof) {
-    auto urdf = ros2_control_test_assets::urdf_head + rpi_pwm_hardware_interface_2dof_ +
-                ros2_control_test_assets::urdf_tail;
-    ASSERT_NO_THROW(hardware_interface::ResourceManager rm(urdf));
+   auto urdf = ros2_control_test_assets::urdf_head + rpi_pwm_hardware_interface_2dof_ +
+            ros2_control_test_assets::urdf_tail;
+   ASSERT_NO_THROW(hardware_interface::ResourceManager rm(urdf));
 }
