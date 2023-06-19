e32dc082ac9e00834cf7d6dfa0aa28c7a57badb0
Atticus Russell <60147768+atticusrussell@users.noreply.github.com>
2023-06-19 16:20:21 -0400

    4 eliminate ament uncrustify from colcon test (#17)
    * disable colcon testing formatting
    * 2 spaces per indent

--- include/rpi_pwm_hardware_interface/angular_servo.hpp
+++ include/rpi_pwm_hardware_interface/angular_servo.hpp
@@ -15,30 +15,30 @@
 #pragma once
 
 class Servo {
-   public:
-      Servo(int pi, int pin);
-      int getPulseWidth();
-      void setPulseWidth(int pulseWidth);
+  public:
+    Servo(int pi, int pin);
+    int getPulseWidth();
+    void setPulseWidth(int pulseWidth);
 
-      int __pi;  // TODO(anyone) rename to indicate public, and pigpio pi
+    int __pi;  // TODO(anyone) rename to indicate public, and pigpio pi
 
-   protected:
-      int __pin;
+  protected:
+    int __pin;
 };
 
 class AngularServo : public Servo {
-   public:
-      AngularServo(int pi, int pin, float minAngle, float maxAngle, int minPulseWidthUs,
-               int maxPulseWidthUs);
-      void setAngle(float angle);
-      int getAngle();
+  public:
+    AngularServo(int pi, int pin, float minAngle, float maxAngle, int minPulseWidthUs,
+        int maxPulseWidthUs);
+    void setAngle(float angle);
+    int getAngle();
 
-   protected:
-      float __angle;
-      float __minAngle;
-      float __maxAngle;
-      int __minPulseWidthUs;
-      int __maxPulseWidthUs;
+  protected:
+    float __angle;
+    float __minAngle;
+    float __maxAngle;
+    int __minPulseWidthUs;
+    int __maxPulseWidthUs;
 };
 
 bool isPigpiodRunning();
